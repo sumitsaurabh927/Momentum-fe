abd0b70292d8c6394caed62de467762fafe65766
sumitsaurabh927 <sumitsaurabh927@gmail.com>
2023-03-28 12:23:50 +0530

    added signup page and route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Routes,Route} from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
 
     </div>
---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import "../Login/login.css";
+
+const Signup = () => {
+    const [formValue,setFormValue]=useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:"",
+        confirmPassword:"",    
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) =>{
+        setFormValue({
+            ...formValue,
+            [e.target.name]:e.target.value,
+        })
+      
+    }
+
+    const handleClick = () =>{
+        navigate("/home");
+    }
+  return (
+    <div className='auth_container'>
+          <form onSubmit={(e)=>e.preventDefault()} className="auth_form">
+            <div className="auth_sub_container">
+              <label for="firstname" >FirstName</label>
+              <label for="lastname" >LastName</label>
+            </div>
+          <div className='auth_sub_container'>
+            <input 
+              type="text" 
+              className="auth_input"
+              id="firstname" 
+              value={formValue.firstName} 
+              name="firstName" 
+              onChange={handleChange} 
+              placeholder="Firstname" 
+            />
+            <input 
+              type="text" 
+              className="auth_input"
+              id="lastname" 
+              value={formValue.lastName} 
+              name="lastName" 
+              onChange={handleChange} 
+              placeholder="Lastname" 
+            />
+          </div>
+            <label for="email" >Email</label>
+            <input 
+              type="text" 
+              className="auth_input"
+              id="email" 
+              value={formValue.email} 
+              name="email" 
+              onChange={handleChange} 
+              placeholder="Enter Your Email" 
+            />
+            <label for="password">Password</label>
+            <input 
+              type="password" 
+              className="auth_input"
+              id="password" 
+              value={formValue.password} 
+              name="password"
+              onChange={handleChange} 
+              placeholder="Enter Your Password" 
+            />
+            <label for="confirmpassword" >ConfirmPassword</label>
+            <input 
+              type="text" 
+              className="auth_input"
+              id="confirmpassword" 
+              value={formValue.confirmPassword} 
+              name="confirmPassword" 
+              onChange={handleChange} 
+              placeholder="Re-Enter you password" 
+            />
+            <div className="auth_button_container">
+              <button onClick={handleClick} className="auth_button" disabled={!(formValue.email && formValue.password)}>Signup</button>
+            </div>
+            <Link to="/">
+              <p>Already have an account? Login here</p>
+            </Link>
+          </form>
+      </div>
+
+  )
+}
+
+export default Signup
