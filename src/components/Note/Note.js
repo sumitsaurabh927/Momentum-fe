f06254cbb07dcd3fca8409bacfb8db603399232c
sumitsaurabh927 <sumitsaurabh927@gmail.com>
2023-03-30 08:23:28 +0530

    added strike-through feature

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./note.css"
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import { deleteNote } from '../../actions/notes';
 
 const Note = ({item,setCurrentId}) => {
+  const [isDone,setIsDone]=useState(false);
   const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const storedIsDone = localStorage.getItem(item._id);
+    if (storedIsDone !== null) {
+      setIsDone(JSON.parse(storedIsDone));
+    }
+  },[item._id])
+
+  const donehandler=()=>{
+    setIsDone(true);
+    localStorage.setItem(item._id, JSON.stringify(true));
+  }
+
   return (
     <div className="note">
-      <h2 className="note_title">{item.title}</h2>
-      <p className='note_description'>{item.description}</p>
+      <h2 className={isDone ? "note_title done" : "note_title"}>{item.title}</h2>
+      <p className={isDone ? "note_description done" : "note_description"}>{item.description}</p>
       <div className="note_button_container">
+        <button className="note_done note_button" onClick={donehandler}>Done</button>
         <button className="note_update note_button" onClick={()=>setCurrentId(item._id)}>Update</button>
         <button className="note_delete note_button" onClick={()=>dispatch(deleteNote(item._id))}>Delete</button>
       </div>
