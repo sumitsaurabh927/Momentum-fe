73374a06dcef29e4f4e9c0b1a629b4bfc77e8f67
sumitsaurabh927 <sumitsaurabh927@gmail.com>
2023-04-04 17:14:46 +0530

    made changes for a note

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,9 +3,15 @@ import "./note.css"
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import { deleteNote } from '../../actions/notes';
+import {MdOutlineEmail} from "react-icons/md";
+import {BsTrash3Fill} from "react-icons/bs"
+import {FiEdit} from "react-icons/fi";
+import {MdSms} from "react-icons/md";
 
 const Note = ({item,setCurrentId}) => {
   const [isDone,setIsDone]=useState(false);
+  const [isEmail,setIsEmail]=useState(false);
+  const [isSms,setIsSms]=useState(false);
   const dispatch = useDispatch();
 
   useEffect(()=>{
@@ -15,21 +21,64 @@ const Note = ({item,setCurrentId}) => {
     }
   },[item._id])
 
-  const donehandler=()=>{
+  const donehandler = () => {
     setIsDone(true);
     localStorage.setItem(item._id, JSON.stringify(true));
   }
 
+  const smsHandler = () => {
+    setIsSms((prev)=>!prev);
+  }
+
+  const emailHandler = () => {
+    setIsEmail((prev)=>!prev);
+  }
+
   return (
     <div className="note">
-      <h2 className={isDone ? "note_title done" : "note_title"}>{item.title}</h2>
-      <p className={isDone ? "note_description done" : "note_description"}>{item.description}</p>
-      <div className="note_button_container">
-        <button className="note_done note_button" onClick={donehandler}>Done</button>
-        <button className="note_update note_button" onClick={()=>setCurrentId(item._id)}>Update</button>
-        <button className="note_delete note_button" onClick={()=>dispatch(deleteNote(item._id))}>Delete</button>
+      <div className='note_container'>
+        <div className='note_text_container'>
+          <input
+            type="checkbox"
+            classname="note_checkbox"
+            checked={isDone}
+            onChange={donehandler}
+          />
+          <h2 className={isDone ? "note_title done" : "note_title"}>{item.title}</h2>
+        </div>
+        <div className="note_button_container">
+          <div className='icon_container' onClick={emailHandler}>
+            <MdOutlineEmail/>
+          </div>
+          <div className='icon_container' onClick={smsHandler}>
+            <MdSms/>
+          </div>
+          <div className='icon_container note_update' onClick={()=>setCurrentId(item._id)}>
+            <FiEdit/>
+          </div>
+          <div className='icon_container note_delete' onClick={()=>dispatch(deleteNote(item._id))}>
+            <BsTrash3Fill/>
+          </div>
+        </div>
       </div>
-      <span>created on {moment(item.createdAt).fromNow()}</span>
+      <div className='note_input_container'>
+        <p className={isDone ? "note_description done" : "note_description"}>{item.description}</p>
+        {isEmail && (
+          <input
+            className='input_box'
+            type="text"
+            placeholder='Enter Assignee email'
+          />
+        )}
+        {isSms && (
+          <input
+            className='input_box'
+            type="number"
+            placeholder='Enter Number'
+          />
+        )}
+        <span>created on {moment(item.createdAt).fromNow()}</span>
+      </div>
     </div>
 
   )
