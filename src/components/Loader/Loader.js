565440c85b3df7e03edd35b6e92b889ab634a259
sumitsaurabh927 <sumitsaurabh927@gmail.com>
2023-04-04 09:09:07 +0530

    added Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Loader.css"
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader_inner"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,12 +3,15 @@ import { Link, useNavigate } from 'react-router-dom'
 import "./login.css";
 import { useDispatch } from 'react-redux';
 import { signin } from '../../actions/auth';
+import Loader from '../../components/Loader/Loader';
 
 const Login = () => {
     const [formValue,setFormValue]=useState({
         email:"",
         password:""
     })
+
+    const [isLoading,setIsLoading]=useState(false);
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -23,7 +26,10 @@ const Login = () => {
 
     const handleFormSubmit=(e)=>{
       e.preventDefault();
-      dispatch(signin(formValue,navigate));
+      setIsLoading(true);
+      dispatch(signin(formValue,navigate)).finally(()=>{
+        setIsLoading(false);
+      });
     }
   
 
@@ -51,7 +57,13 @@ const Login = () => {
               placeholder="Enter Your Password" 
             />
             <div className="auth_button_container">
-              <button  className="auth_button" disabled={!(formValue.email && formValue.password)}>Login</button>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <button className="auth_button" disabled={!(formValue.email && formValue.password)}>
+                Login
+              </button>
+            )}
             </div>
             <Link to="/signup">
               <p>Don't have an account? Signup here</p>
